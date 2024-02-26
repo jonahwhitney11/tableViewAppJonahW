914719b96f9933dea5160cc7ab595a08253252ae
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-02-26 13:29:53 -0600

    Table View app, initial commit

--- tableViewApp/ViewController.swift
+++ tableViewApp/ViewController.swift
@@ -7,11 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var petTable: UITableView!
+    
+    let petArray = ["cat", "dog", "parakeet", "snake", "frog", "mouse", "turtle", "fist", "rat", "ferret", "lizard", "rabbit"]
+    
+    let cellID = "cellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        petTable.dataSource = self
+        petTable.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if (cell == nil) {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellID)
+        }
+        cell?.textLabel?.text = petArray[indexPath.row]
+        return cell!
     }
 
 
